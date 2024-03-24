8bae207aa655693cf378589e69c5e33d624b2c7c
Ninad More <ninadmore183@gmail.com>
2024-03-24 18:46:34 +0530

    Checking

--- medezz/lib/main.dart
+++ medezz/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
+  // This widget is the root of your application
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
